324690a2aa7f62bd5b3c9f9e18bde3a2c0177b65
Pierre-Yves <git@pywebdesign.net>
2015-03-14 16:01:14 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ sudo docker-compose run web ls
 sudo docker-compose run web bundle install
 echo "Please specify your gem name:"
 read name
-sudo docker-compose run web rails plugin new $(name)
+sudo docker-compose run web rails plugin new "$name"
